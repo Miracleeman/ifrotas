fe269d7b5399fb778dbeb3ca581bdcf1c76f6a3d
miracleeman2@yahoo.com <miracleeman2@yahoo.com>
2025-10-20 17:46:44 -0300

    mudando portas Vercel

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import serverless from 'serverless-http'
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello from Railway + Node!");
 });
 
-const PORT = process.env.PORT || 8080;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
